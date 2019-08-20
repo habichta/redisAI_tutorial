c8874defa20add55a74521fe3ae93d74bdd5a4f6
Arthur <arthurhabicht88@gmail.com>
2019-08-20 15:41:34 +0200

    [added] tf 2.0 redisAI

--- mnist_redisAI.py
+++ mnist_redisAI.py
@@ -2,26 +2,27 @@ import redisai as rai
 import ml2rt
 import numpy as np
 import tensorflow as tf
+from tensorflow import keras
 from skimage import io
 import json
 con=rai.Client()
-#model = ml2rt.load_model('reference_model/3/resnet50.pb')
-filepath = 'data/guitar.jpg'
-numpy_img = io.imread(filepath).astype(dtype=np.float32)
-numpy_img = np.expand_dims(numpy_img, axis=0) / 255
+model = ml2rt.load_model('reference_model/1/saved_model.pb')
+print(type(model))
 
+model = keras.experimental.load_from_saved_model('reference_model/1')
+print(type(model))
+#con.modelset('mnist_model',rai.Backend.tf, rai.Device.cpu, input=['input'], output=['output'], data=model)
 
-#con.modelset('model',rai.Backend.tf, rai.Device.cpu, input=['images'], output=['output'], data=model)
-
-
+'''
 con.tensorset('images', numpy_img)
 con.modelrun(
-    'model',
-    input=['images'],
-    output=['output']
+'model',
+input=['images'],
+output=['output']
 )
 ret = con.tensorget('output', as_type=rai.BlobTensor).to_numpy()
 classes_idx = json.load(open('data/imagenet_classes.json'))
 ind = ret.argmax()
 print(ind, ret.shape)
 print(classes_idx[str(ind-1)])
+'''
