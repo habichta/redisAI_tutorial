9488fc45390396c05b973dea125eb090f171a19f
Arthur <arthurhabicht88@gmail.com>
2019-08-20 14:03:03 +0200

    [cleanup] unnecessary code

--- redisAI.py
+++ redisAI.py
@@ -6,8 +6,6 @@ from skimage import io
 import json
 con=rai.Client()
 #model = ml2rt.load_model('reference_model/3/resnet50.pb')
-#model = tf.saved_model.load('reference_model/2')
-#print(model)
 filepath = 'data/guitar.jpg'
 numpy_img = io.imread(filepath).astype(dtype=np.float32)
 numpy_img = np.expand_dims(numpy_img, axis=0) / 255
@@ -17,7 +15,6 @@ print(numpy_img)
 #con.modelset('model',rai.Backend.tf, rai.Device.cpu, input=['images'], output=['output'], data=model)
 
 
-#input_shape = rai.Tensor(rai.DType.float, shape=[3], value=[224,224,3])
 con.tensorset('images', numpy_img)
 con.modelrun(
     'model',
